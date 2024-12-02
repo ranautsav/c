4df106a51cd8278b8e44b9e1d8d70366160e554b
ranautsav <ranautsav666@gmail.com>
2024-12-02 15:02:25 +0530

    Add files via upload

---
+++ 8989-ranautsav-exam/q1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    for (char ch = 'A'; ch <= 'Z'; ch++)
+    {
+        printf("\n%c ", ch);
+    }
+
+    return 0; 
+}
+
+
+    
---
+++ 8989-ranautsav-exam/q3.c
@@ -0,0 +1,10 @@
+
+#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=1;i<=15;i++)
+    printf("\n%d",i);
+
+    return 0;
+}
